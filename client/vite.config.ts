538b14b112f1725c805bd872a8f11d5fe2bd086c
dianr1108 <dianr1108@gmail.com>
2025-10-28 18:08:35 +0700

    Change build output directory to root repo
    Updated output directory for build to point to the root repo.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',
+  root: '.',                 // pakai folder client sebagai root
   build: {
-    outDir: 'dist/public',
-  },
+    outDir: '../dist/public', // keluarkan ke dist/public di ROOT repo
+    emptyOutDir: true
+  }
 })
